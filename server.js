b9fdff6ca64f394ee30cb62d803ce2d11f9ae3b9
Adya Agarwal <adyaagrawal008@gmail.com>
2026-04-30 11:33:12 +0530

    fix port for railway

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import taskRoutes from './routes/tasks.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
